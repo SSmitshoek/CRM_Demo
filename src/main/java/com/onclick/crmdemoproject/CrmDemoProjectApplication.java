edaf454c035ca6b4b4b1bb51d8d956cd7ab465de
Stefan Smitshoek <stefan@onclicksolutions.nl>
2023-06-02 14:40:15 +0200

    initialize project

---
+++ src/main/java/com/onclick/crmdemoproject/CrmDemoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.onclick.crmdemoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmDemoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmDemoProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/onclick/crmdemoproject/CrmDemoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onclick.crmdemoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmDemoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
